a61a45aa18ac4830219c3832fac1c86041d20b85
Petro Lehtonen <petro.lehtonen@gmail.com>
2022-10-20 14:53:18 +0300

    refaktorointi tehty

---
+++ app.js
@@ -0,0 +1,20 @@
+const { mongoUrl } = require('./utils/config')
+const logger = require('./utils/logger')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+
+mongoose.connect(mongoUrl).then( () => {
+    logger.info('connected to MongoDB')
+}).catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+})
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-const { PORT, mongoUrl} = require('./utils/config')
-const logger = require('./utils/logger')
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const Blog = require('./models/blog')
-const blogsRouter = require('./controllers/blogs')
+const { PORT } = require('./utils/config')
+const logger = require('./utils/logger')
 
-mongoose.connect(mongoUrl)
+const server = http.createServer(app)
 
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
-
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
-})
+server.listen(PORT, () => {
+    logger.info(`Server running on port ${PORT}`)
+  })
